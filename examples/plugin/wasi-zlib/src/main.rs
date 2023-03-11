347a878edc1082fdc58fd588f54e1eb1e34986bb
Divy Srivastava <dj.srivastava23@gmail.com>
2023-03-11 13:44:40 +0530

    try to expose a raw C api

--- examples/plugin/wasi-zlib/src/main.rs
+++ examples/plugin/wasi-zlib/src/main.rs
@@ -1,12 +1,49 @@
 mod ffi {
   #[link(wasm_import_module = "zlib")]
   extern "C" {
-      pub fn func_add(a: i32, b: i32) -> i32;
+      pub fn adler32(adler: u32, buf: *const u8, len: u32) -> u32;
+      pub fn deflateInit(strm: *mut super::z_stream, level: i32, version: *const u8, stream_size: i32) -> i32;
+      pub fn deflate(strm: *mut super::z_stream, flush: i32) -> i32;
   }
 }
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct z_stream {
+    pub next_in: *const u8,
+    pub avail_in: i32,
+    pub total_in: i32,
+    pub next_out:  *const u8,
+    pub avail_out: i32,
+    pub total_out: i32,
+    pub msg:  *const u8,
+    pub state:  *const u8,
+    pub zalloc:  *const u8,
+    pub zfree:  *const u8,
+    pub opaque:  *const u8,
+    pub data_type: i32,
+    pub adler: i32,
+    pub reserved: i32,
+}
+
 fn main() {
-  let a = 1;
-  let b = 2;
-  let c = unsafe { ffi::func_add(a, b) };
-  println!("{} + {} = {}", a, b, c);
+  let adler = unsafe { ffi::adler32(1, "hello".as_ptr(), 5) };
+  println!("adler32: {}", adler);
+  
+  let mut strm = unsafe { std::mem::zeroed::<z_stream>() };
+  let version = "1.2.11".as_ptr();
+  let status = unsafe { ffi::deflateInit(&mut strm, -1, version, std::mem::size_of::<z_stream>() as i32) };
+  println!("deflateInit: {}", status);
+
+  let input = b"hello";
+  strm.next_in = input.as_ptr();
+  strm.avail_in = input.len() as _;
+
+  let mut output = [0u8; 1024];
+  strm.next_out = &mut output as *mut _;
+  strm.avail_out = output.len() as _;
+
+  let status = unsafe { ffi::deflate(&mut strm, 0) };
+  println!("deflate: {}", status);
+  println!("output: {:?}", &output[..strm.total_out as usize]);
 }
--- plugins/wasi_zlib/src/lib.rs
+++ plugins/wasi_zlib/src/lib.rs
@@ -4,26 +4,211 @@ use wasmedge_sys::{
 };
 use wasmedge_types::{error::HostFuncError, ValType};
 
-fn extern_add(
-  _: CallingFrame,
+struct ValueConverter(WasmValue);
+
+impl From<ValueConverter> for i32 {
+  fn from(v: ValueConverter) -> Self {
+    v.0.to_i32()
+  }
+}
+
+impl From<ValueConverter> for u32 {
+  fn from(v: ValueConverter) -> Self {
+    v.0.to_i32() as _
+  }
+}
+
+impl From<ValueConverter> for *const u8 {
+  fn from(v: ValueConverter) -> Self {
+    v.0.to_i32() as _
+  }
+}
+
+impl From<ValueConverter> for u64 {
+  fn from(v: ValueConverter) -> Self {
+    v.0.to_i32() as _
+  }
+}
+
+fn frame_memory(
+  frame: &CallingFrame,
+  index: u32,
+  offset: u32,
+) -> Result<*mut u8, HostFuncError> {
+  let mut memory = frame.memory_mut(0).unwrap();
+  let data = memory.data_pointer_mut(index, offset).unwrap();
+  Ok(data)
+}
+
+fn w_adler32(
+  frame: CallingFrame,
   inputs: Vec<WasmValue>,
 ) -> Result<Vec<WasmValue>, HostFuncError> {
-  let val1 = if inputs[0].ty() == ValType::ExternRef {
-    inputs[0]
-  } else {
-    return Err(HostFuncError::User(2));
-  };
-  let val1 = val1
-    .extern_ref::<i32>()
-    .expect("fail to get i32 from an ExternRef");
-  dbg!(val1);
-  let val2 = if inputs[1].ty() == ValType::I32 {
-    inputs[1].to_i32()
-  } else {
-    return Err(HostFuncError::User(3));
+  let adler = ValueConverter(inputs[0]).into();
+  let start = ValueConverter(inputs[1]).into();
+  let offset = ValueConverter(inputs[2]).into();
+
+  let ptr = frame_memory(&frame, start, offset)?;
+
+  let checksum = unsafe { libz_sys::adler32(adler, ptr, offset) };
+
+  Ok(vec![WasmValue::from_i32(checksum as _)])
+}
+
+fn w_crc32(
+  frame: CallingFrame,
+  inputs: Vec<WasmValue>,
+) -> Result<Vec<WasmValue>, HostFuncError> {
+  let crc = ValueConverter(inputs[0]).into();
+  let start = ValueConverter(inputs[1]).into();
+  let offset = ValueConverter(inputs[2]).into();
+
+  let ptr = frame_memory(&frame, start, offset)?;
+
+  let checksum = unsafe { libz_sys::crc32(crc, ptr, offset) };
+
+  Ok(vec![WasmValue::from_i32(checksum as _)])
+}
+
+mod zlib_32 {
+  use super::*;
+  mod flate2_libz_helpers {
+    // Workaround for https://github.com/rust-lang/libz-sys/issues/55
+    // See https://github.com/rust-lang/flate2-rs/blob/31fb07820345691352aaa64f367c1e482ad9cfdc/src/ffi/c.rs#L60
+    use std::os::raw::c_void;
+    use std::{
+      alloc::{self, Layout},
+      ptr,
+    };
+
+    const ALIGN: usize = std::mem::align_of::<usize>();
+
+    fn align_up(size: usize, align: usize) -> usize {
+      (size + align - 1) & !(align - 1)
+    }
+
+    pub extern "C" fn zalloc(
+      _ptr: *mut c_void,
+      items: u32,
+      item_size: u32,
+    ) -> *mut c_void {
+      // We need to multiply `items` and `item_size` to get the actual desired
+      // allocation size. Since `zfree` doesn't receive a size argument we
+      // also need to allocate space for a `usize` as a header so we can store
+      // how large the allocation is to deallocate later.
+      let size = match (items as usize)
+        .checked_mul(item_size as usize)
+        .map(|size| align_up(size, ALIGN))
+        .and_then(|i| i.checked_add(std::mem::size_of::<usize>()))
+      {
+        Some(i) => i,
+        None => return ptr::null_mut(),
+      };
+
+      // Make sure the `size` isn't too big to fail `Layout`'s restrictions
+      let layout = match Layout::from_size_align(size, ALIGN) {
+        Ok(layout) => layout,
+        Err(_) => return ptr::null_mut(),
+      };
+
+      unsafe {
+        // Allocate the data, and if successful store the size we allocated
+        // at the beginning and then return an offset pointer.
+        let ptr = alloc::alloc(layout) as *mut usize;
+        if ptr.is_null() {
+          return ptr as *mut c_void;
+        }
+        *ptr = size;
+        ptr.add(1) as *mut c_void
+      }
+    }
+
+    pub extern "C" fn zfree(_ptr: *mut c_void, address: *mut c_void) {
+      unsafe {
+        // Move our address being free'd back one pointer, read the size we
+        // stored in `zalloc`, and then free it using the standard Rust
+        // allocator.
+        let ptr = (address as *mut usize).offset(-1);
+        let size = *ptr;
+        let layout = Layout::from_size_align_unchecked(size, ALIGN);
+        alloc::dealloc(ptr as *mut u8, layout)
+      }
+    }
+  }
+
+  #[repr(transparent)]
+  #[derive(Debug, Copy, Clone)]
+  pub struct Ptr<T>(u32, std::marker::PhantomData<T>);
+
+  const _: () = {
+    assert!(std::mem::size_of::<Ptr<()>>() == 4);
   };
 
-  Ok(vec![WasmValue::from_i32(val1 + val2)])
+  #[repr(C)]
+  #[derive(Debug, Copy, Clone)]
+  pub struct z_stream {
+    pub next_in: Ptr<u8>,
+    pub avail_in: i32,
+    pub total_in: i32,
+    pub next_out: Ptr<u8>,
+    pub avail_out: i32,
+    pub total_out: i32,
+    pub msg: Ptr<u8>,
+    pub state: Ptr<()>,
+    pub zalloc: Ptr<()>,
+    pub zfree: Ptr<()>,
+    pub opaque: Ptr<()>,
+    pub data_type: i32,
+    pub adler: i32,
+    pub reserved: i32,
+  }
+
+  impl z_stream {
+    pub fn to_native(&self, frame: &CallingFrame) -> libz_sys::z_stream {
+      libz_sys::z_stream {
+        next_in: frame_memory(frame, self.next_in.0, 0).unwrap() as _,
+        avail_in: self.avail_in as _,
+        total_in: self.total_in as _,
+        next_out: frame_memory(frame, self.next_out.0, 0).unwrap() as _,
+        avail_out: self.avail_out as _,
+        total_out: self.total_out as _,
+        msg: frame_memory(frame, self.msg.0, 0).unwrap() as _,
+        state: frame_memory(frame, self.state.0, 0).unwrap() as _,
+        zalloc: flate2_libz_helpers::zalloc,
+        zfree: flate2_libz_helpers::zfree,
+        opaque: frame_memory(frame, self.opaque.0, 0).unwrap() as _,
+        data_type: self.data_type as _,
+        adler: self.adler as _,
+        reserved: self.reserved as _,
+      }
+    }
+  }
+}
+
+fn w_deflateInit(
+  frame: CallingFrame,
+  inputs: Vec<WasmValue>,
+) -> Result<Vec<WasmValue>, HostFuncError> {
+  let strm: u32 = ValueConverter(inputs[0]).into();
+  let level: i32 = ValueConverter(inputs[1]).into();
+  let version: u32 = ValueConverter(inputs[2]).into();
+  let stream_size: i32 = ValueConverter(inputs[3]).into();
+
+  let strm = frame_memory(&frame, strm, 0)?;
+  let version = frame_memory(&frame, version, 0)?;
+  let strm = unsafe { &mut *(strm as *mut zlib_32::z_stream) };
+
+  let mut strm64 = strm.to_native(&frame);
+  let status = unsafe {
+    libz_sys::deflateInit_(
+      &mut strm64 as *mut _,
+      level,
+      libz_sys::zlibVersion(),
+      std::mem::size_of::<libz_sys::z_stream>() as _,
+    )
+  };
+
+  Ok(vec![WasmValue::from_i32(status as _)])
 }
 
 unsafe extern "C" fn instance_create(
@@ -32,12 +217,22 @@ unsafe extern "C" fn instance_create(
   let mut import =
     std::mem::ManuallyDrop::new(ImportModule::create("zlib").unwrap());
   let func_ty = FuncType::create(
-    vec![ValType::ExternRef, ValType::I32],
+    vec![ValType::I32, ValType::I32, ValType::I32],
     vec![ValType::I32],
   )
   .unwrap();
-  let host_func = Function::create(&func_ty, Box::new(extern_add), 0).unwrap();
-  import.add_func("func_add", host_func);
+  let adler32 = Function::create(&func_ty, Box::new(w_adler32), 0).unwrap();
+  let crc32 = Function::create(&func_ty, Box::new(w_crc32), 0).unwrap();
+  import.add_func("adler32", adler32);
+  import.add_func("crc32", crc32);
+  let func_ty = FuncType::create(
+    vec![ValType::I32, ValType::I32, ValType::I32, ValType::I32],
+    vec![ValType::I32],
+  )
+  .unwrap();
+  let deflateInit =
+    Function::create(&func_ty, Box::new(w_deflateInit), 0).unwrap();
+  import.add_func("deflateInit", deflateInit);
   import.as_ptr() as *mut _
 }
 
@@ -59,7 +254,7 @@ pub extern "C" fn init() -> *const ffi::WasmEdge_PluginDescriptor {
       Patch: 0,
       Build: 0,
     },
-    ModuleCount: 0,
+    ModuleCount: MODULES.len() as _,
     ProgramOptionCount: 0,
     ModuleDescriptions: MODULES.as_ptr() as _,
     ProgramOptions: std::ptr::null_mut(),
